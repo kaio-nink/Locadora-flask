25c4050805b3c415d0bdde35ac32e3a00f3c1e6d
Harison Santos <harison_vr@hotmail.com>
2022-10-19 19:49:45 -0300

    procedure car registration

--- locadora/cadastrarCarro.sql
+++ locadora/cadastrarCarro.sql
@@ -1,5 +1,5 @@
 delimiter ;;
-create procedure cadastrarCarro(in id_carro int, in disponivel boolean)
+create procedure cadastrarCarro(in tipo varchar(15), in modelo varchar(15), in marca varchar(10), in ano varchar(4), in disponivel boolean)
     begin
-        insert into carro(disponivel) values(disponivel);
+        insert into carro(tipo, modelo, marca, ano, disponivel) values(tipo, modelo, marca, ano, disponivel);
     end;;
