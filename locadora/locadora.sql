a701cdf7e080a1fdf1065b9907632b0bf8a98ebd
Harison Santos <harison_vr@hotmail.com>
2022-10-19 18:59:15 -0300

    last att

--- locadora/locadora.sql
+++ locadora/locadora.sql
@@ -6,10 +6,18 @@ create table cliente
     primary key(id_cliente)
 );
 
+create table tipo_carro
+(
+    modelo varchar(15),
+    valorDiaria numeric(4,2),
+    valorSemanal numeric(7,2),
+    primary key(modelo)
+);
+
 create table carro
 (
     id_carro int auto_increment,
-    modelo varchar(15)
+    modelo varchar(15),
     marca varchar(10),        
     ano varchar(4),
     disponivel Boolean not null,   
@@ -17,20 +25,12 @@ create table carro
     foreign key(modelo) references tipo_carro(modelo)
 );
 
-create table tipo_carro
-(
-    modelo varchar(10),
-    valorDiaria numeric(4,2),
-    valorSemanal numeric(7,2),
-    primary key(modelo)
-);
-
 create table aluguel
 (
     id_cliente int,
     id_carro int,
     dataInicial date,
-    dataFinal date,
+    numDias int,
     primary key(id_cliente, id_carro, dataInicial),
     foreign key(id_cliente) references cliente(id_cliente),
     foreign key(id_carro) references carro(id_carro)
