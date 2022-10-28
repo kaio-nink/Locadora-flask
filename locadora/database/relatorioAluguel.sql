e24ebd53501cf63569b9201ac64375279d6060f0
Harison Santos <58005145+hsantos97@users.noreply.github.com>
2022-10-28 17:43:58 -0300

    update

--- locadora/database/relatorioAluguel.sql
+++ locadora/database/relatorioAluguel.sql
@@ -1,4 +1,4 @@
-create view relatorioAluguel(tipoCarro, modelo, nomeCliente, dataIni, dataRetorno, valor) if not exists as
+create view relatorioAluguel(tipoCarro, modelo, nomeCliente, dataIni, dataRetorno) if not exists as
 (
     select tipo, concat(marca , " " , modelo, " " , ano) as veiculo, nome, dataInicial, numDias, date_add(dataInicial, interval numDias day) as dataRetorno
     from aluguel natural join carro natural join tipo_carro natural join cliente
